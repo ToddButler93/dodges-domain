15f4ada23b6df49c15c84d9e6ccfec853bea7cde
Dodge <toddbutler93@outlook.com.au>
2023-11-02 01:04:50 +1100

    Adjust navigation gap so text isn't cut off by page title.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,7 @@ export const Header = () => {
     return (
         <header className="drop-shadow-header sticky top-0 z-50 flex justify-between bg-zinc-800 py-4 px-2 font-mono font-medium text-slate-50 backdrop-blur md:px-5 min-w-full">
             <div className="flex flex-col items-center justify-between gap-3 text-m font-medium text-slate-50 md:flex-row min-w-full">
-                <div className="flex items-center justify-start">
+                <div className="flex items-center justify-start sm:gap-6 px-2 md:px-18">
                     <Link href="/" className="flex items-center text-2xl font-bold text-slate-50">
                         <Image src={logo} alt="Dodges Domain" className="w-8 h-8"/>
                         <p className="px-2 ">
