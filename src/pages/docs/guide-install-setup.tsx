44e0d6da71263003e102a8674dd4f6dec7a02510
Dodge <toddbutler93@outlook.com.au>
2023-11-02 00:12:10 +1100

    Draft of install guide

--- src/pages/docs/guide-install-setup.tsx
+++ src/pages/docs/guide-install-setup.tsx
@@ -1,7 +1,6 @@
 import Head from "next/head";
 import Link from "next/link";
-
-import { api } from "~/utils/api";
+import { Button, Callout } from "@tremor/react";
 
 export default function Home() {
 
@@ -16,33 +15,43 @@ export default function Home() {
         <h1 className="text-5xl">Installation</h1>
         <h2 className="text-2xl">Step 1 - Install via Steam</h2>
         <div className="flex flex-col gap-3">
-            <Link href="steam://run/17080/">
-                Install via Steam
-            </Link>
+          <div className="flex gap-3">
+            <iframe src="https://store.steampowered.com/widget/17080/" width="100%" height="190"></iframe>
+          </div>
             Or
             <Link href="https://f000.backblazeb2.com/file/tribes-zip/Tribes_Ascend_Parting_Gifts.zip">
-                Manual Download
+              <Button size="xl" color="teal">
+              Manual Download
+              </Button>
             </Link>
         </div>
         <h2 className="text-4xl">Step 2 - Download the Tribes Launcher</h2>
         <div className="flex flex-col gap-3">
-            <p className="text-teal-500">
-            Note:
-            Make sure to extract this before running!
-            </p>
+              <Callout
+              title="Note"
+              color="teal"
+            >
+              Make sure to extract this before running!
+            </Callout>
+            
             <Link href="https://github.com/mcoot/TribesLauncherSharp/releases/latest">
-                Download Tribes Launcher Sharp
+              <Button size="xl" color="teal">
+              Download Tribes Launcher Sharp
+              </Button>
             </Link>
-            <p className="text-teal-500">
-            Info:
-            You will need the <Link href="https://aka.ms/vs/17/release/vc_redist.x86.exe">Microsoft Visual C++ x86 Redistributes</Link> if you don't have them already installed.
-            </p>
+            
+            <Callout
+              title="Info"
+              color="teal"
+            >
+              You will need the <Link href="https://aka.ms/vs/17/release/vc_redist.x86.exe">Microsoft Visual C++ x86 Redistributes</Link> if you don't have them already installed.
+            </Callout>
         </div>
         <h2 className="text-4xl">Step 3 - Download the Tribes Launcher</h2>
         <div className="flex flex-col gap-3">
-        <p>Extract and Launch the Tribes Launcher.</p>
-        <p>In the Tribes Launcher, select the Community for your login server, hit Login and inject TAMods.</p>
-        <p>Once injected, the Play Now button will show whether you are in OOTB or GOTY mode. There is a button on the main menu to switch. You need to be in GOTY mode to play on GOTY servers and OOTB mode to play on OOTB servers.</p>
+          <p>Extract and Launch the Tribes Launcher.</p>
+          <p>In the Tribes Launcher, select the Community for your login server, hit Login and inject TAMods.</p>
+          <p>Once injected, the Play Now button will show whether you are in OOTB or GOTY mode. There is a button on the main menu to switch. You need to be in GOTY mode to play on GOTY servers and OOTB mode to play on OOTB servers.</p>
         </div>
       </div>
     </>
