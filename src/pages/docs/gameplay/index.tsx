1c7b92d774e24ba7d11bb73b0037dccd691fa940
Dodge <toddbutler93@outlook.com.au>
2023-11-02 22:33:55 +1100

    Removed Alt placeholder for images

--- src/pages/docs/gameplay/index.tsx
+++ src/pages/docs/gameplay/index.tsx
@@ -41,7 +41,7 @@ export default function Home() {
                 width={500}
                 height={500}
                 style={{ objectFit: "contain" }}
-                alt="Picture of the author"
+
               />
             </Card>
           </Link>
@@ -74,7 +74,7 @@ export default function Home() {
                 width={500}
                 height={500}
                 style={{ objectFit: "contain" }}
-                alt="Picture of the author"
+
               />
             </Card>
           </Link>
@@ -109,7 +109,7 @@ export default function Home() {
                 width={500}
                 height={500}
                 style={{ objectFit: "contain" }}
-                alt="Picture of the author"
+
               />
             </Card>
           </Link>
--- src/pages/docs/guide-install-setup.tsx
+++ src/pages/docs/guide-install-setup.tsx
@@ -33,7 +33,7 @@ export default function Home() {
                   width={500}
                   height={500}
                   style={{ objectFit: "contain" }}
-                  alt="Picture of the author"
+  
                 />
               </Link>
               <Link className="w-full" href="steam://install/17080/">
