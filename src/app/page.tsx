d1adb9917adf6b8a405d88a1459f65485560623a
Dodge <toddbutler93@outlook.com.au>
2023-11-13 00:03:44 +1100

    Added hero video to start page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 import Link from "next/link";
-import Image from "next/image";
 
-import { Button } from "@mantine/core";
+import { Button, Group } from "@mantine/core";
+
+import Image from "next/image";
 
 import { api } from "~/trpc/server";
 
@@ -12,6 +13,42 @@ export default async function Home() {
 
   return (
     <>
+      <div className="relative">
+        <div className="bg-hero-section flex max-h-[50] w-screen items-center bg-cover bg-center bg-no-repeat">
+          <video
+            inset-0
+            object-cover
+            width="100%"
+            autoPlay={true}
+            muted
+            loop
+            preload="metadata"
+          >
+            <source
+              src="/static/video/kata-ds-bounce.webm"
+              type="video/webm"
+            ></source>
+            Your browser does not support .webm video.
+          </video>
+          <Group className="absolute inset-0 flex flex-col items-center justify-center">
+            <Image
+              alt=""
+              src="/static/img/tribes-ascend-logo.png"
+              width={500}
+              height={500}
+              className=" max-w-md"
+            />
+            <Button
+              component={Link}
+              href="/docs/guide-install-setup"
+              size="xl"
+              radius="xl"
+            >
+              Install Tribes Ascend
+            </Button>
+          </Group>
+        </div>
+      </div>
       <div className="max-w-8xl flex flex-col py-12 text-center ">
         <div className="max-w-8xl div div-col gap-6 px-32 text-center text-6xl">
           {data?.map((post) => (
@@ -20,15 +57,6 @@ export default async function Home() {
             </div>
           ))}
         </div>
-        <h1 className="py-6 text-6xl">Dodge&apos;s Domain</h1>
-        <Button
-          component={Link}
-          href="/docs/guide-install-setup"
-          size="xl"
-          radius="xl"
-        >
-          Install Tribes Ascend
-        </Button>
       </div>
       <div className="max-w-8xl ">
         <div className="flex flex-col gap-12 md:flex-row">
