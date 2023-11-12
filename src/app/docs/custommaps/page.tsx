7147adc05f2ad34fdf7cc8ff83193eab899b951a
Dodge <toddbutler93@outlook.com.au>
2023-11-12 22:42:59 +1100

    Removed links from custom map page.

--- src/app/docs/custommaps/page.tsx
+++ src/app/docs/custommaps/page.tsx
@@ -35,25 +35,19 @@ export default function CustomMaps() {
         <div className="flex flex-col  gap-6 md:flex-row">
           <Card className="mx-auto max-w-xl">
             <p className=" text-center text-3xl">Acheron</p>
-            <Link href="/docs/custommaps/trctf-acheron">
               <Image
                 alt=""
                 radius="md"
                 src="/static/img/maps/acheron-river.png"
               />
-            </Link>
           </Card>
           <Card className="mx-auto max-w-xl">
             <p className=" text-center text-3xl">Andromeda</p>
-            <Link href="/docs/custommaps/trctf-andromeda">
               <Image alt="" radius="md" src="/static/img/maps/andromeda2.jpg" />
-            </Link>
           </Card>
           <Card className="mx-auto max-w-xl">
             <p className=" text-center text-3xl">Ascent</p>
-            <Link href="/docs/custommaps/trctf-ascent">
               <Image alt="" radius="md" src="/static/img/maps/ascent2.jpg" />
-            </Link>
           </Card>
         </div>
       </Flex>
@@ -61,29 +55,23 @@ export default function CustomMaps() {
         <div className="flex flex-col  gap-6 md:flex-row">
           <Card className="mx-auto max-w-xl">
             <p className=" text-center text-3xl">Blues</p>
-            <Link href="/docs/custommaps/trctf-blues">
               <Image
                 alt=""
                 radius="md"
                 src="/static/img/maps/blues-v011.jpeg"
               />
-            </Link>
           </Card>
           <Card className="mx-auto max-w-xl">
             <p className=" text-center text-3xl">Crash</p>
-            <Link href="/docs/custommaps/trctf-crash">
               <Image alt="" radius="md" src="/static/img/maps/ascent2.jpg" />
-            </Link>
           </Card>
           <Card className="mx-auto max-w-xl">
             <p className=" text-center text-3xl">Deserted Valley</p>
-            <Link href="/docs/custommaps/trctf-desertedvalley">
               <Image
                 alt=""
                 radius="md"
                 src="/static/img/maps/deserted-valley-overview.png"
               />
-            </Link>
           </Card>
         </div>
       </Flex>
@@ -91,21 +79,15 @@ export default function CustomMaps() {
         <div className="flex flex-col  gap-6 md:flex-row">
           <Card className="mx-auto max-w-xl">
             <p className=" text-center text-3xl">Eclipse</p>
-            <Link href="/docs/custommaps/trctf-eclipse">
               <Image alt="" radius="md" src="/static/img/maps/eclipse2.jpg" />
-            </Link>
           </Card>
           <Card className="mx-auto max-w-xl">
             <p className=" text-center text-3xl">Fracture</p>
-            <Link href="/docs/custommaps/trctf-fracture">
               <Image alt="" radius="md" src="/static/img/maps/fra.jpg" />
-            </Link>
           </Card>
           <Card className="mx-auto max-w-xl">
             <p className=" text-center text-3xl">Incidamus</p>
-            <Link href="/docs/custommaps/trctf-incidamus">
               <Image alt="" radius="md" src="/static/img/maps/incid.jpg" />
-            </Link>
           </Card>
         </div>
       </Flex>
@@ -113,25 +95,19 @@ export default function CustomMaps() {
         <div className="flex flex-col  gap-6 md:flex-row">
           <Card className="mx-auto max-w-xl">
             <p className=" text-center text-3xl">Meridian</p>
-            <Link href="/docs/custommaps/trctf-meridian">
               <Image alt="" radius="md" src="/static/img/maps/ascent2.jpg" />
-            </Link>
           </Card>
           <Card className="mx-auto max-w-xl">
             <p className=" text-center text-3xl">Oceanus</p>
-            <Link href="/docs/custommaps/trctf-oceanus">
               <Image alt="" radius="md" src="/static/img/maps/oce.jpg" />
-            </Link>
           </Card>
           <Card className="mx-auto max-w-xl">
             <p className=" text-center text-3xl">Periculo</p>
-            <Link href="/docs/custommaps/trctf-periculo">
               <Image
                 alt=""
                 radius="md"
                 src="/static/img/maps/periculo-features.png"
               />
-            </Link>
           </Card>
         </div>
       </Flex>
@@ -139,25 +115,19 @@ export default function CustomMaps() {
         <div className="flex flex-col  gap-6 md:flex-row">
           <Card className="mx-auto max-w-xl">
             <p className=" text-center text-3xl">Phlegethon</p>
-            <Link href="/docs/custommaps/trctf-phlegethon">
               <Image
                 alt=""
                 radius="md"
                 src="/static/img/maps/phlegathon-river.png"
               />
-            </Link>
           </Card>
           <Card className="mx-auto max-w-xl">
             <p className=" text-center text-3xl">Polaris</p>
-            <Link href="/docs/custommaps/trctf-polaris">
               <Image alt="" radius="md" src="/static/img/maps/polaris2.jpg" />
-            </Link>
           </Card>
           <Card className="mx-auto max-w-xl">
             <p className=" text-center text-3xl">Styx</p>
-            <Link href="/docs/custommaps/trctf-styx/">
               <Image alt="" radius="md" src="/static/img/maps/sty.jpg" />
-            </Link>
           </Card>
         </div>
       </Flex>
@@ -168,15 +138,11 @@ export default function CustomMaps() {
         <div className="flex flex-col  gap-6 md:flex-row">
           <Card className="mx-auto max-w-xl">
             <p className=" text-center text-3xl">Elysian Battleground</p>
-            <Link href="/docs/custommaps/trarena-elysianbattleground">
               <Image alt="" radius="md" src="/static/img/maps/eb.jpg" />
-            </Link>
           </Card>
           <Card className="mx-auto max-w-xl">
             <p className=" text-center text-3xl">Ruins</p>
-            <Link href="/docs/custommaps/trarena-ruins">
               <Image alt="" radius="md" src="/static/img/maps/ruins2.jpg" />
-            </Link>
           </Card>
         </div>
       </Flex>
@@ -188,15 +154,11 @@ export default function CustomMaps() {
         <div className="flex flex-col  gap-6 md:flex-row">
           <Card className="mx-auto max-w-xl">
             <p className=" text-center text-3xl">Broadside</p>
-            <Link href="/docs/custommaps/trctfblitz-broadside">
               <Image alt="" radius="md" src="/static/img/maps/ascent2.jpg" />
-            </Link>
           </Card>
           <Card className="mx-auto max-w-xl">
             <p className=" text-center text-3xl">Air Arena</p>
-            <Link href="/docs/custommaps/trctfblitz-airarena">
               <Image alt="" radius="md" src="/static/img/maps/ascent2.jpg" />
-            </Link>
           </Card>
         </div>
       </Flex>
@@ -208,15 +170,11 @@ export default function CustomMaps() {
         <div className="flex flex-col  gap-6 md:flex-row">
           <Card className="mx-auto max-w-xl">
             <p className=" text-center text-3xl">Treacherous Pass</p>
-            <Link href="/docs/custommaps/trctf-treacherouspass">
               <Image alt="" radius="md" src="/static/img/maps/ascent2.jpg" />
-            </Link>
           </Card>
           <Card className="mx-auto max-w-xl">
             <p className=" text-center text-3xl">Maze Runner</p>
-            <Link href="/docs/custommaps/trctfblitz-mazerunner">
               <Image alt="" radius="md" src="/static/img/maps/ascent2.jpg" />
-            </Link>
           </Card>
         </div>
       </Flex>
