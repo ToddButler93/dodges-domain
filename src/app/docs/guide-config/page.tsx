965d2b3d6f0992a78ed06c44a9e588e9f21bea8a
Dodge <toddbutler93@outlook.com.au>
2024-04-25 23:19:27 +1000

    Update Config Page

--- src/app/docs/guide-config/page.tsx
+++ src/app/docs/guide-config/page.tsx
@@ -1,225 +1,179 @@
-import {
-  Button,
-  Code,
-  Title,
-  Image,
-  Text,
-  Card,
-  Container,
-  Paper,
-  SimpleGrid,
-  Alert,
-  List,
-  ListItem,
-  Space,
-} from "@mantine/core";
 import Link from "next/link";
+import { Alert } from "~/components/ui/alert";
+import { Button } from "~/components/ui/button";
+import { Card } from "~/components/ui/card";
+import { Separator } from "~/components/ui/separator";
 
 export default function GuideConfig() {
   return (
     <>
-      <div className="flex max-w-6xl flex-col gap-6 py-3">
-        <Paper className="flex flex-col gap-6 p-3 shadow-xl">
+      <div className="flex flex-col gap-6">
+        <div className="flex flex-col gap-6">
           <Card className="grow py-6 text-center shadow-xl">
-            <Title>Tribes Ascend Configuration</Title>
+            <h1>Tribes Ascend Configuration</h1>
           </Card>
-          <Container size="xl">
-            <Container size="sm">
-              <Text className="text-xl">
-                Your <Code className=" text-xl">Tribes.ini</Code> is the config
-                file for your graphical settings. It is located under{" "}
-                <Code>Documents\My Games\Tribes Ascend\TribesGame\Config\</Code>
-              </Text>
-            </Container>
-            <SimpleGrid cols={{ base: 1, sm: 2 }}>
-              <Card className="flex flex-col gap-3 shadow-xl">
+          <div className="flex flex-col gap-6">
+            <p className="text-xl">
+              Your <b className=" text-xl">Tribes.ini</b> is the config file for
+              your graphical settings. It is located under{" "}
+              <b>Documents\My Games\Tribes Ascend\TribesGame\Config\</b>
+            </p>
+            <div className="grid grid-cols-2 gap-3">
+              <Card className="flex flex-col gap-3 p-3 shadow-xl">
                 <Link href="/static/text/dodges-ini/tribes.ini">
-                  <Image
+                  <img
                     src="/static/img/dodge-ini.png"
-                    alt='Dodges tribes.ini'
+                    alt="Dodges tribes.ini"
                   />
                 </Link>
 
-                <Button
-                  component={Link}
-                  href="/static/text/dodges-ini/tribes.ini"
-                  size="xl"
-                  radius="xl"
-                  styles={{
-                    inner: { fontSize: 24 },
-                  }}
-                >
-                  Tribes.ini
+                <Button asChild>
+                  <Link href="/static/text/dodges-ini/tribes.ini">
+                    Tribes.ini
+                  </Link>
                 </Button>
-                <Text className="text-xl">
-                  This <Code>Tribes.ini</Code> is for people with weaker systems
-                  or who are after a competitive edge.
-                </Text>
+                <p className="text-xl">
+                  This <b>Tribes.ini</b> is for people with weaker systems or
+                  who are after a competitive edge.
+                </p>
               </Card>
-              <Card className="flex flex-col gap-3 shadow-xl">
+              <Card className="flex flex-col gap-3 p-3 shadow-xl">
                 <Link href="/static/text/dodges-pretty-ini/tribes.ini">
-                  <Image
+                  <img
                     src="/static/img/dodge-pretty-ini.png"
-                    alt='Pretty tribes.ini'
+                    alt="Pretty tribes.ini"
                   />
                 </Link>
-                <Button
-                  component={Link}
-                  href="/static/text/dodges-pretty-ini/tribes.ini"
-                  size="xl"
-                  radius="xl"
-                  styles={{
-                    inner: { fontSize: 24 },
-                  }}
-                >
-                  &quot;Pretty&quot; Tribes.ini
+                <Button asChild>
+                  <Link href="/static/text/dodges-pretty-ini/tribes.ini">
+                    &quot;Pretty&quot; Tribes.ini
+                  </Link>
                 </Button>
-                <Text className="text-xl">
-                  This <Code>Tribes.ini</Code> is for people with a super-strong
+                <p className="text-xl">
+                  This <b>Tribes.ini</b> is for people with a super-strong
                   system and prioritize looks over a competitive edge.
-                </Text>
+                </p>
               </Card>
-            </SimpleGrid>
-            <Space h="md" />
-            <Container size="sm">
+            </div>
+            <Separator />
+            <div>
               <Alert title="Note" color="teal">
-                <Text className="text-start text-xl">
+                <p className="text-start text-xl">
                   All ini&quot;s provided are for 1080p resolution. Manually
-                  alter this by changing <Code>ResX=1920</Code> and{" "}
-                  <Code>ResY=1080</Code> to your native monitor resolution. HUD
-                  Elements can be toggled with the{" "}
+                  alter this by changing <b>ResX=1920</b> and <b>ResY=1080</b>{" "}
+                  to your native monitor resolution. HUD Elements can be toggled
+                  with the{" "}
                   <Link href={"#in-game-settings"}>in game settings</Link>.
-                </Text>
+                </p>
               </Alert>
-            </Container>
-            <Space h="md" />
-            <SimpleGrid cols={{ base: 1, sm: 2 }}>
-              <Card className="flex flex-col gap-3 shadow-xl">
-                <Title order={3}>Required Tweaks</Title>
-                <List listStyleType="disc" className="pe-6">
-                  <ListItem>
-                    Set all instances of <Code>bForceStaticTerrain=False</Code>{" "}
-                    to <Code>bForceStaticTerrain=True</Code>. This will stop
-                    terrain from popping in.
-                  </ListItem>
-                  <ListItem>
-                    Set <Code>OneFrameThreadLag=False</Code>. This can help with
-                    mouse responsiveness.
-                  </ListItem>
-                  <ListItem>
-                    Set <Code>m_bTinyWeaponsEnabled=True</Code>. This will make
-                    your weapons take up less screen space.
-                  </ListItem>
-                </List>
+            </div>
+            <Separator />
+            <div className="grid grid-cols-2 gap-3">
+              <Card className="flex flex-col gap-3 p-3 shadow-xl">
+                <h3>Required Tweaks</h3>
+                <ul className="list-disc p-3">
+                  <li>
+                    Set all instances of <b>bForceStaticTerrain=False</b> to{" "}
+                    <b>bForceStaticTerrain=True</b>. This will stop terrain from
+                    popping in.
+                  </li>
+                  <li>
+                    Set <b>OneFrameThreadLag=False</b>. This can help with mouse
+                    responsiveness.
+                  </li>
+                  <li>
+                    Set <b>m_bTinyWeaponsEnabled=True</b>. This will make your
+                    weapons take up less screen space.
+                  </li>
+                </ul>
               </Card>
-              <Card className="flex flex-col gap-3 shadow-xl">
-                <Title order={3}>Recommended Tweaks</Title>
-                <List withPadding listStyleType="disc" className="pe-6">
-                  <ListItem>
-                    Set <Code>Bloom=True</Code> to <Code>Bloom=False</Code>
-                  </ListItem>
-                  <ListItem>
-                    Set <Code>MotionBlur=True</Code> to{" "}
-                    <Code>MotionBlur=False</Code>
-                  </ListItem>
-                  <ListItem>
-                    To set a frame rate cap, set{" "}
-                    <Code>bSmoothFrameRate=true</Code> and
-                    <Code>MaxSmoothedFrameRate=X</Code> where X is the frame
-                    rate your wish to cap your game at.
-                  </ListItem>
-                  <ListItem>
-                    Set FogVolumes=False to make smoke clouds smaller.
-                  </ListItem>
-                  <ListItem>
-                    Set <Code>SpeedTreeLeaves=False</Code> and
-                    <Code>SpeedTreeFronds=False</Code> to hide tree leaves and
-                    some branches.
-                  </ListItem>
-                  <ListItem>
-                    Set <Code>AllowRadialBlur=False</Code> to remove some of the
+              <Card className="flex flex-col gap-3 p-3 shadow-xl">
+                <h3>Recommended Tweaks</h3>
+                <ul className="list-disc p-3">
+                  <li>
+                    Set <b>Bloom=True</b> to <b>Bloom=False</b>
+                  </li>
+                  <li>
+                    Set <b>MotionBlur=True</b> to <b>MotionBlur=False</b>
+                  </li>
+                  <li>
+                    To set a frame rate cap, set <b>bSmoothFrameRate=true</b>{" "}
+                    and
+                    <b>MaxSmoothedFrameRate=X</b> where X is the frame rate your
+                    wish to cap your game at.
+                  </li>
+                  <li>Set FogVolumes=False to make smoke clouds smaller.</li>
+                  <li>
+                    Set <b>SpeedTreeLeaves=False</b> and
+                    <b>SpeedTreeFronds=False</b> to hide tree leaves and some
+                    branches.
+                  </li>
+                  <li>
+                    Set <b>AllowRadialBlur=False</b> to remove some of the
                     distortions from weapon impacts.
-                  </ListItem>
-                  <ListItem>
-                    Set <Code>Borderless=False</Code> to improve input lag on
-                    certain systems.
-                  </ListItem>
-                </List>
+                  </li>
+                  <li>
+                    Set <b>Borderless=False</b> to improve input lag on certain
+                    systems.
+                  </li>
+                </ul>
               </Card>
-            </SimpleGrid>
-          </Container>
-        </Paper>
-        <Paper className="flex flex-col gap-6 shadow-xl p-3">
-          <Container size="sm">
-            <div>
-              <Title order={2}>TribesInput.ini</Title>
-              <div>
-                <Button
-                  component={Link}
-                  href="/static/text/dodges-input-ini/tribesinput.ini"
-                  size="xl"
-                  radius="xl"
-                  styles={{
-                    inner: { fontSize: 24 },
-                  }}
-                >
-                  Dodges TribesInput.ini
-                </Button>
-              </div>
-              <Space h="sm"></Space>
-              <Text className="text-xl">
-                Your <Code>TribesInput.ini</Code> is the config file for your
-                key bindings.
-              </Text>
-              <Text className="text-xl">
-                The main thing I&quot;d make sure is that you have
-                <Code>bEnableMouseSmoothing</Code> set to <Code>false</Code>.
-              </Text>
-              <Text className="text-xl">
-                It is located under &quot;Documents\My Games\Tribes
-                Ascend\TribesGame\Config\&quot;
-              </Text>
-              <Text className="text-xl">
-                A sensitivity of approximately 4.501 at maximum zoom will cause
-                issues with mouse input. 5+ sensitivity is recommended.
-              </Text>
-              <Text className="text-xl">
-                This is my personal <Code>TribesInput.ini</Code>. Contains some
-                of the autocompletes for official and custom server admins.
-              </Text>
-              <Text className="text-xl">
-                You must set this to <Code>Read Only</Code> otherwise the game
-                will overwrite the file.
-              </Text>
             </div>
-          </Container>
-        </Paper>
-        <Paper className="flex flex-col gap-6 shadow-xl p-3">
-          <Container size="sm">
-            <Title order={2}>TAMods</Title>
+          </div>
+        </div>
+        <div className="flex flex-col gap-6 p-3 shadow-xl">
+          <h2>TribesInput.ini</h2>
+          <div>
+            <Button asChild>
+              <Link href="/static/text/dodges-input-ini/tribesinput.ini">
+                Dodges TribesInput.ini
+              </Link>
+            </Button>
+          </div>
+          <Separator></Separator>
+          <p className="text-xl">
+            Your <b>TribesInput.ini</b> is the config file for your key
+            bindings.
+          </p>
+          <p className="text-xl">
+            The main thing I&quot;d make sure is that you have
+            <b>bEnableMouseSmoothing</b> set to <b>false</b>.
+          </p>
+          <p className="text-xl">
+            It is located under &quot;Documents\My Games\Tribes
+            Ascend\TribesGame\Config\&quot;
+          </p>
+          <p className="text-xl">
+            A sensitivity of approximately 4.501 at maximum zoom will cause
+            issues with mouse input. 5+ sensitivity is recommended.
+          </p>
+          <p className="text-xl">
+            This is my personal <b>TribesInput.ini</b>. Contains some of the
+            autocompletes for official and custom server admins.
+          </p>
+          <p className="text-xl">
+            You must set this to <b>Read Only</b> otherwise the game will
+            overwrite the file.
+          </p>
+        </div>
+        <div className="flex flex-col gap-6 p-3 shadow-xl">
+          <h2>TAMods</h2>
 
-            <div>
-              <Button
-                component={Link}
-                href="/static/text/dodges-config-lua/config.lua"
-                size="xl"
-                radius="xl"
-                styles={{
-                  inner: { fontSize: 24 },
-                }}
-              >
+          <div>
+            <Button asChild>
+              <Link href="/static/text/dodges-config-lua/config.lua">
                 Dodges TAMods Config.lua
-              </Button>
-            </div>
-            <Space h="sm"></Space>
-            <Text className="text-xl">
-              It is recommended to delete the default crosshair and either use
-              the Tribes Ascend crosshair or create your own. If you choose to
-              create your own I&quot;d recommend disabling the default crosshair
-              within ubermenu so you keep hit markers.
-            </Text>
-          </Container>
-        </Paper>
+              </Link>
+            </Button>
+          </div>
+          <Separator></Separator>
+          <p className="text-xl">
+            It is recommended to delete the default crosshair and either use the
+            Tribes Ascend crosshair or create your own. If you choose to create
+            your own I&quot;d recommend disabling the default crosshair within
+            ubermenu so you keep hit markers.
+          </p>
+        </div>
       </div>
     </>
   );
