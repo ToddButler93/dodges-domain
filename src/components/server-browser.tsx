42e6d7433d8e2c84891fc32a2ab1a5b768140ff2
Dodge <toddbutler93@outlook.com.au>
2024-04-27 15:02:08 +1000

    Began work on server browser front-end, shadcn-ified

---
+++ src/components/server-browser.tsx
@@ -0,0 +1,85 @@
+import type { Key } from "react";
+import type { ServerInfo } from "~/server/api/routers/loginServers";
+import { api } from "~/trpc/server";
+import { Card } from "./ui/card";
+import { Badge } from "./ui/badge";
+
+export default async function ServerBrowserComponent() {
+  const sortedPugServers = await api.pugLoginData.getPUGLoginData.query();
+
+  const sortedCommunityServers =
+    await api.communityLoginData.getCommunityLoginData.query();
+
+  if (sortedPugServers == null && sortedCommunityServers == null)
+    return <div>The PUG login server is currently unavailable.</div>;
+
+  return (
+    <>
+      {sortedPugServers || sortedCommunityServers ? (
+        <div className="flex max-w-7xl flex-col gap-6 py-6">
+          <div className="gap-3 p-3 shadow">
+            <h1 className=" p-center pb-6">PUG Login Server</h1>
+            <div className="grid grid-cols-1 gap-3 sm:grid-cols-2 lg:grid-cols-3 ">
+              {sortedPugServers
+                .filter((server: ServerInfo) => server.name && server.map)
+                .map((server: ServerInfo, index: Key) => (
+                  <Card className="shadow" key={index}>
+                    <div>
+                      <img alt="" src="/static/img/maps/acheron-river.png" />
+                    </div>
+                    <p>{server.name}</p>
+                    <p>
+                      {server.type} | {server.map}
+                    </p>
+                    <div className="grid grid-flow-col">
+                      <Badge color="pink">PUG</Badge>
+                      {server.players && server.players.length > 0 ? (
+                        <>
+                          <Badge color="teal">
+                            Players: {server.players.length}
+                          </Badge>
+                        </>
+                      ) : (
+                        <Badge color="red">Players: 0</Badge>
+                      )}
+                    </div>
+                  </Card>
+                ))}
+            </div>
+          </div>
+          <div className="gap-3 p-6 shadow">
+            <h1 className="p-center pb-6">Community Login Server</h1>
+            <div className="grid grid-cols-1 gap-3 sm:grid-cols-2 lg:grid-cols-3 "></div>
+            {sortedCommunityServers
+              .filter((server: ServerInfo) => server.name && server.map)
+              .map((server: ServerInfo, index: Key) => (
+                <Card key={index}>
+                  <div>
+                    <img alt="" src="/static/img/maps/acheron-river.png" />
+                  </div>
+                  <p>{server.name}</p>
+                  <p>
+                    {server.type} | {server.map}
+                  </p>
+                  <div className="grid grid-flow-col justify-between">
+                    <Badge color="blue">Community</Badge>
+                    {server.players && server.players.length > 0 ? (
+                      <>
+                        <Badge color="teal">
+                          Players: {server.players.length}
+                        </Badge>
+                      </>
+                    ) : (
+                      <Badge color="red">Players: 0</Badge>
+                    )}
+                  </div>
+                </Card>
+              ))}
+          </div>
+        </div>
+      ) : (
+        <Card>Loading...</Card>
+      )}
+    </>
+  );
+}
