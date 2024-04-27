af480cad1ded05112aec4b316ea8dda8905a978c
Dodge <toddbutler93@outlook.com.au>
2024-04-27 15:47:37 +1000

    Clean up map dev page, fix some spacing issues

--- src/app/docs/development/guide-map-dev/page.tsx
+++ src/app/docs/development/guide-map-dev/page.tsx
@@ -11,7 +11,7 @@ export default function GuideMapDevelopment() {
         <Card className="grow py-6 text-center">
           <h1>Map Development Guide</h1>
         </Card>
-        <div className="flex flex-col gap-3 p-6">
+        <div className="flex flex-col gap-3">
           <Alert className="flex flex-col gap-3 p-3">
             If you are interested in map development I recommend joining the
             Tribes Ascend Community discord and asking for assistance. We are
@@ -23,7 +23,7 @@ export default function GuideMapDevelopment() {
             </Button>
           </Alert>
           <h2>UDK Setup</h2>
-          <h3> Step 1</h3>
+          <h3>Step 1</h3>
           <p>
             You need to download and install{" "}
             <Link href="http://library.theexiled.pwnageservers.com/file.php?id=2964">
@@ -39,9 +39,9 @@ export default function GuideMapDevelopment() {
             >
               TA actor classes
             </Link>
-            . Extract the <b>TribesGame</b> folder to
+            . Extract the <b>TribesGame</b> folder to{" "}
             <b>\UDK\UDK-2011-01\Development\Src\</b> and the{" "}
-            <b>TA_BaseAssets.upk & AUD_MUS_CTF.upk</b> to
+            <b>TA_BaseAssets.upk & AUD_MUS_CTF.upk</b> to{" "}
             <b>\UDK\UDK-2011-01\UDKGame\Content\</b>
           </p>
           <p>
@@ -58,7 +58,7 @@ export default function GuideMapDevelopment() {
             It is recommended to use the latest map file releases to give
             yourself a head start with assets:
           </p>
-          <ul>
+          <ul  className="list-disc p-3">
             <li>
               <Link
                 passHref={true}
@@ -173,11 +173,11 @@ export default function GuideMapDevelopment() {
             </li>
           </ul>
           <p>
-            These zip files have a directory structure which is
+            These zip files have a directory structure which is{" "}
             <b>!TRIBESDIR\TribesGame\CookedPC\Maps\</b>
           </p>
           <p>
-            All .upk files will go into your
+            All .upk files will go into your{" "}
             <b>\UDK\UDK-2011-01\UDKGame\Content\</b>folder. Where as the maps
             can go into <b>\UDK\UDK-2011-01\UDKGame\Content\Maps</b>
           </p>
@@ -194,7 +194,7 @@ export default function GuideMapDevelopment() {
             , it is best to copy these now before you delete the downloaded
             archive.
           </p>
-          <h3> Step 2</h3>
+          <h3>Step 2</h3>
           <p>
             After UDK is installed, go to{" "}
             <b>\UDK\UDK-2011-01\UDKGame\Config\</b> and open{" "}
@@ -203,29 +203,29 @@ export default function GuideMapDevelopment() {
             new line write <b>ModEditPackages=TribesGame</b> Save the file and
             close the editor.
           </p>
-          <h3> Step 3</h3>
+          <h3>Step 3</h3>
           <p>
             Launch the UDK Editor, when asked to update the outdated scripts hit{" "}
             <strong>yes</strong>. Wait for the success message then close it and
             reopen UDK Editor to get started.
           </p>
-          <h2> Required Knowledge</h2>
+          <h2>Required Knowledge</h2>
           <p>
             Do not save over one of the downloaded package files. If you want to
             add custom pures/meshes to your map you should create a new package
             file.
           </p>
           <p>
-            Map files(.fmap) require specific file naming to work in Tribes
+            Map files (.fmap) require specific file naming to work in Tribes
             Ascend. <b>TrGameMode-MapName</b> where the CTF map Arx Novena is
             named <b>TrCTF-ArxNovena</b>
           </p>
         </div>
         <Separator />
-        <div className="flex flex-col gap-3 p-6">
-          <h2> Your First Map</h2>
-          <h3> Step 1</h3>
-          <ul>
+        <div className="flex flex-col gap-3">
+          <h2>Your First Map</h2>
+          <h3>Step 1</h3>
+          <ul  className="list-disc p-3">
             <li>
               <Link
                 passHref={true}
@@ -250,17 +250,17 @@ export default function GuideMapDevelopment() {
           <p>
             Launch UDK and open the map you wish to edit in your Maps folder.
           </p>
-          <h3> Step 2</h3>
+          <h3>Step 2</h3>
           <p>
             Now you are free to do editing however you see fit. I recommend
             modifying the terrain, adding material to the terrain and adding a
             couple of random static meshes from the TA packages within the
             content browser to get yourself familiar with UDK. When you feel
-            like you are ready to test the map out. Hit <b>Build</b>then
+            like you are ready to test the map out. Hit <b>Build</b>then{" "}
             <b>Build All</b> Hit File then Save Current Level. Close the UDK
             Editor and save any asset packages <strong>you</strong> created.
           </p>
-          <h3> Step 3</h3>
+          <h3>Step 3</h3>
           <p>
             You will need to complete Step 1 of{" "}
             <Link href="#refshadercache-compiler-error">
@@ -272,7 +272,7 @@ export default function GuideMapDevelopment() {
             Open <b>\UDK\UDK-2011-01\BinariesUnrealFrontend.exe</b>
           </p>
           <p>
-            Select the profile <b>DM-Deck</b>and remove
+            Select the profile <b>DM-Deck</b>and remove{" "}
             <b>DM-Deck.udk</b>under <b>Maps to Cook</b> Add your custom map,
             click Cook and then Cook Packages. Exit Unreal Frontend once
             completed.
@@ -280,9 +280,9 @@ export default function GuideMapDevelopment() {
           <p>Now your map should be ready to be tested!</p>
         </div>
         <Separator />
-        <div className="flex flex-col gap-3 p-6">
-          <h2> Testing Maps</h2>
-          <h3> Launching Custom Maps on Leaked Dev Build</h3>
+        <div className="flex flex-col gap-3">
+          <h2>Testing Maps</h2>
+          <h3>Launching Custom Maps on Leaked Dev Build</h3>
           <p>
             You need the{" "}
             <Link href="http://library.theexiled.pwnageservers.com/category.php?id=200">
@@ -296,8 +296,8 @@ export default function GuideMapDevelopment() {
               dev client launcher
             </Link>
           </p>
-          <h4> Easy way</h4>
-          <h5> Step 1</h5>
+          <h4>Easy way</h4>
+          <h5>Step 1</h5>
           <p>
             Extract the{" "}
             <Link
@@ -306,25 +306,25 @@ export default function GuideMapDevelopment() {
             >
               TA package files
             </Link>{" "}
-            to the dev builds <b>\TribesGame\CookedPC\Maps\</b>folder.
+            to the dev builds <b>\TribesGame\CookedPC\Maps\</b> folder.
           </p>
-          <h5> Step 2</h5>
+          <h5>Step 2</h5>
           <p>
-            Copy <b>RefShaderCache-PC-D3D-SM3.upk</b>in
-            <b>C:\UDK\UDK-2011-01\UDKGame\CookedPC\</b>and paste it into
+            Copy <b>RefShaderCache-PC-D3D-SM3.upk</b> in
+            <b>C:\UDK\UDK-2011-01\UDKGame\CookedPC\</b> and paste it into
             <b>\Tribes\TribesGame\CookedPC\</b>
           </p>
           <p>
             Copy the uncooked map from
-            <b>\UDK\UDK-2011-01\UDKGame\Content\Maps\</b>to
+            <b>\UDK\UDK-2011-01\UDKGame\Content\Maps\</b> to
             <b>\Tribes\TribesGame\CookedPC\Maps\</b>
           </p>
           <p>
             Copy any asset packages you&apos;ve created from
-            <b>\UDK\UDK-2011-01\UDKGame\Content\</b>to
+            <b>\UDK\UDK-2011-01\UDKGame\Content\</b> to
             <b>\Tribes\TribesGame\CookedPC\Maps\</b>
           </p>
-          <h5> Step 3</h5>
+          <h5>Step 3</h5>
           <p>
             Download and launch the{" "}
             <Link href="http://library.theexiled.pwnageservers.com/file.php?id=2942">
@@ -341,15 +341,15 @@ export default function GuideMapDevelopment() {
             dropdown.
           </p>
           <p>Note: TrCTF-BasicMap</p>
-          <h5> Step 5</h5>
+          <h5>Step 5</h5>
           <p>Click Launch.</p>
           <p>
             Once the game has launched hit the <kbd>`</kbd> key and type
-            <b>ChangeTeam BloodEagle</b>for blood eagle spawns or
-            <b>ChangeTeam DiamondSword</b>for diamond sword spawns.
+            <b>ChangeTeam BloodEagle</b> for blood eagle spawns or
+            <b>ChangeTeam DiamondSword</b> for diamond sword spawns.
           </p>
-          <h4> Hard way</h4>
-          <h5> Step 1</h5>
+          <h4>Hard way</h4>
+          <h5>Step 1</h5>
           <p>
             Extract the{" "}
             <Link
@@ -358,17 +358,17 @@ export default function GuideMapDevelopment() {
             >
               TA package files
             </Link>
-            to dev builds <b>\TribesGame\CookedPC\Maps\</b>folder.
+            to dev builds <b>\TribesGame\CookedPC\Maps\</b> folder.
           </p>
-          <h5> Step 2</h5>
+          <h5>Step 2</h5>
           <p>
-            Copy <b>RefShaderCache-PC-D3D-SM3.upk</b>in
-            <b>C:\UDK\UDK-2011-01\UDKGame\CookedPC\</b>and paste it into
+            Copy <b>RefShaderCache-PC-D3D-SM3.upk</b> in{" "}
+            <b>C:\UDK\UDK-2011-01\UDKGame\CookedPC\</b> and paste it into{" "}
             <b>\Tribes\TribesGame\CookedPC\</b>
           </p>
           <p>
             Copy the uncooked map from
-            <b>\UDK\UDK-2011-01\UDKGame\Content\Maps\</b>to
+            <b>\UDK\UDK-2011-01\UDKGame\Content\Maps\</b> to
             <b>\Tribes\TribesGame\CookedPC\Maps\</b>
           </p>
           <p>
@@ -376,7 +376,7 @@ export default function GuideMapDevelopment() {
             <b>\UDK\UDK-2011-01\UDKGame\Content\</b>to
             <b>\Tribes\TribesGame\CookedPC\Maps\</b>
           </p>
-          <h5> Step 3</h5>
+          <h5>Step 3</h5>
           <p>
             Open Command Prompt. CD to the Tribes dev builds
             <b>\Binaries\Win32\</b>Folder and enter
@@ -386,7 +386,7 @@ export default function GuideMapDevelopment() {
             Note: <b>TrGameMode-MapName</b>for Arx Novena would be
             <b>TrCTF-ArxNovena</b>
           </p>
-          <h5> Step 4</h5>
+          <h5>Step 4</h5>
           <p>Hit Enter.</p>
           <p>
             Once the game has launched hit the <kbd>`</kbd>key and type
@@ -394,7 +394,7 @@ export default function GuideMapDevelopment() {
             <b>ChangeTeam DiamondSword</b>for diamond sword spawns. Otherwise,
             you will spawn as neither team.
           </p>
-          <h3> Hosting Custom Map</h3>
+          <h3>Hosting Custom Map</h3>
           <p>
             <Link
               passHref={true}
@@ -415,13 +415,13 @@ export default function GuideMapDevelopment() {
             to the servers <b>\Tribes\TribesGame\CookedPC\Maps\</b>folder.
           </p>
           <p>
-            Copy <b>RefShaderCache-PC-D3D-SM3.upk</b>in
-            <b>C:\UDK\UDK-2011-01\UDKGame\CookedPC\</b>and paste it into
+            Copy <b>RefShaderCache-PC-D3D-SM3.upk</b> in{" "}
+            <b>C:\UDK\UDK-2011-01\UDKGame\CookedPC\</b>and paste it into{" "}
             <b>\Tribes\TribesGame\CookedPC\</b>
           </p>
           <p>
-            Copy the uncooked map from
-            <b>\UDK\UDK-2011-01\UDKGame\Content\Maps\</b>to
+            Copy the uncooked map from{" "}
+            <b>\UDK\UDK-2011-01\UDKGame\Content\Maps\</b>to{" "}
             <b>\Tribes\TribesGame\CookedPC\Maps\</b>
           </p>
           <p>
@@ -432,10 +432,10 @@ export default function GuideMapDevelopment() {
           </p>
         </div>
         <Separator />
-        <div className="flex flex-col gap-3 p-6">
-          <h2> External Resources</h2>
+        <div className="flex flex-col gap-3">
+          <h2>External Resources</h2>
           <p>
-            Ex Hi-Rez Studios map developer Katelyn Pitstick has posted some
+            Ex Hi-Rez Studios map developer Katelyn Pitstick has posted some{" "}
             information specifically about making Tribes Ascend maps on{" "}
             <Link passHref={true} href="https://katelyn-mp.com/wp/?cat=8">
               Kate&apos;s website
@@ -446,9 +446,9 @@ export default function GuideMapDevelopment() {
             thenewboston has an old{" "}
             <Link
               passHref={true}
-              href="https://youtube.com/playul?ul=PLFF967D7CA020E636"
+              href="https://www.youtube.com/watch?v=K7AAgQHJWGc&list=PLFF967D7CA020E636&pp=iAQB"
             >
-              UDK Tutorial Playul
+              UDK Tutorial Playlist
             </Link>{" "}
             where he is using an old 2011 build of UDK. Tutorial 36 is where he
             begins work on terrain.
@@ -491,17 +491,17 @@ export default function GuideMapDevelopment() {
           </p>
         </div>
         <Separator />
-        <div className="flex flex-col gap-3 p-6">
-          <h2> Troubleshooting</h2>
-          <h3> RefShaderCache Compiler Error</h3>
+        <div className="flex flex-col gap-3">
+          <h2>Troubleshooting</h2>
+          <h3>RefShaderCache Compiler Error</h3>
           <p>
             In the official version of the game, the shader compiler crashes
             when attempting to load a custom map in the game.
           </p>
-          <h4> Workaround (Easy way)</h4>
-          <h5> Step 1</h5>
+          <h4>Workaround (Easy way)</h4>
+          <h5>Step 1</h5>
           <p>
-            Download the latest master
+            Download the latest master{" "}
             <Link
               passHref={true}
               href="https://tamods-update.s3.ap-southeast-2.amazonaws.com/packages/master-refshadercache.zip"
@@ -509,53 +509,53 @@ export default function GuideMapDevelopment() {
               RefShaderCache-PC-D3D-SM3.upk
             </Link>
             , paste it into <b>\UDK\UDK-2011-01\UDKGame\Content\</b> If
-            you&apos;ve played on custom maps before, the one from your
-            <b>\Tribes\TribesGame\CookedPC\</b>folder should work.
+            you&apos;ve played on custom maps before, the one from your{" "}
+            <b>\Tribes\TribesGame\CookedPC\</b> folder should work.
           </p>
-          <h5> Step 2</h5>
+          <h5>Step 2</h5>
           <p>
-            Run UnrealFrontEnd and cook the map. Copy
-            <b>RefShaderCache-PC-D3D-SM3.upk</b>in
-            <b>\UDK\UDK-2011-01\UDKGame\CookedPC\</b>and paste it into
+            Run UnrealFrontEnd and cook the map. Copy{" "}
+            <b>RefShaderCache-PC-D3D-SM3.upk</b>in{" "}
+            <b>\UDK\UDK-2011-01\UDKGame\CookedPC\</b>and paste it into{" "}
             <b>\Tribes\TribesGame\CookedPC\</b> This will need to be done every
             time you wish to test the map.
           </p>
-          <h4> Workaround (Advanced users)</h4>
-          <h5> Step 1</h5>
+          <h4>Workaround (Advanced users)</h4>
+          <h5>Step 1</h5>
           <p>
-            Copy the <b>RefShaderCache-PC-D3D-SM3.upk</b>in
-            <b>\Tribes\TribesGame\CookedPC\</b>and paste it into
+            Copy the <b>RefShaderCache-PC-D3D-SM3.upk</b>in{" "}
+            <b>\Tribes\TribesGame\CookedPC\</b>and paste it into{" "}
             <b>\UDK\UDK-2011-01\UDKGame\Content\</b>
           </p>
-          <h5> Step 2</h5>
+          <h5>Step 2</h5>
           <p>
-            Using a hex editor, you will see the values
-            <b>C1 83 2A 9E 25 03 02 00</b>, modify the <b>02</b>in that sequence
+            Using a hex editor, you will see the values{" "}
+            <b>C1 83 2A 9E 25 03 02 00</b>, modify the <b>02</b> in that sequence
             to <b>00</b>
           </p>
-          <h5> Step 3</h5>
+          <h5>Step 3</h5>
           <p>
-            Run UnrealFrontEnd and cook the map. Copy
-            <b>RefShaderCache-PC-D3D-SM3.upk</b>in
-            <b>C:\UDK\UDK-2011-01\UDKGame\CookedPC\</b>and paste it into
+            Run UnrealFrontEnd and cook the map. Copy{" "}
+            <b>RefShaderCache-PC-D3D-SM3.upk</b> in{" "}
+            <b>C:\UDK\UDK-2011-01\UDKGame\CookedPC\</b> and paste it into{" "}
             <b>\Tribes\TribesGame\CookedPC\</b> This will need to be done every
             time you wish to test the map.
           </p>
-          <h4> Reason why this is needed</h4>
+          <h4>Reason why this is needed</h4>
           <p>
             It looks like the shader compiler is missing some required shaders
             to properly compile.
           </p>
           <p>
-            Cooking the map with the unedited RefShaderCache-PC-D3D-SM3.upk will
+            Cooking the map with the unedited <b>RefShaderCache-PC-D3D-SM3.upk</b> will
             fail due to license version checks, the license version is in the
             file’s header which we edit via our hex editor.
           </p>
-          <h3> Where is UDK Installed?</h3>
+          <h3>Where is UDK Installed?</h3>
           <p>
             By default UDK installs to <b>C:\UDK\UDK-2011-01\</b>
           </p>
-          <h3>pure issues of static meshes from the packages</h3>
+          <h3>Texture issues of static meshes from the packages</h3>
           <p>
             `You need to copy over the materials for the meshes you are using in
             your map, otherwise they will show up with a blue and white pure
