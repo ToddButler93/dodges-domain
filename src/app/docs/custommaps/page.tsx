5300da362813e83f36e4c584f0121318f5accc25
Dodge <toddbutler93@outlook.com.au>
2024-04-27 14:23:40 +1000

    Update PUG guide to use Shadcn, adjusted cols

--- src/app/docs/custommaps/page.tsx
+++ src/app/docs/custommaps/page.tsx
@@ -1,155 +1,196 @@
 "use client";
-import Head from "next/head";
 import Link from "next/link";
-import { Image, SimpleGrid, Card, Button } from "@mantine/core";
+import { Button } from "~/components/ui/button";
+import { Card } from "~/components/ui/card";
+import { Separator } from "~/components/ui/separator";
 
 export default function CustomMaps() {
   return (
     <>
-      <Head>
-        <title>Dodges Domain</title>
-        <meta
-          name="description"
-          content="Dodges Domain, the output of his small brain."
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <h1 className="py-6 text-6xl">Community Maps</h1>
-      <Button
-        component={Link}
-        href="/docs/guide-install-setup"
-        size="xl"
-        radius="xl"
-        styles={{
-          root: { backgroundColor: "teal" },
-          label: { color: "white" },
-          inner: { fontSize: 24 },
-        }}
-      >
-        Install Community Maps
-      </Button>
-      <div className="max-w-8xl py-3 text-center text-6xl" color="green">
-        <h1 className="py-6">CTF</h1>
+      <div className="flex flex-col gap-6 px-6">
+        <Card className="grow py-6 text-center">
+          <h1>Tribes Ascend Quick PUG Guide</h1>
+        </Card>
+        <p>
+          <Button asChild>
+            <Link href="/docs/guide-install-setup">Install Community Maps</Link>
+          </Button>
+        </p>
+        <Separator />
+        <Card className="grow py-6 text-center">
+          <h1>CTF</h1>
+        </Card>
+
+        <div className="grid gap-3 sm:grid-cols-2 lg:grid-cols-3">
+          <Card className="mx-auto max-w-xl">
+            <p className=" py-3 text-center text-3xl">Acheron</p>
+            <img
+              alt=""
+              className="rounded"
+              src="/static/img/maps/acheron-river.png"
+            />
+          </Card>
+          <Card className="mx-auto max-w-xl">
+            <p className=" py-3 text-center text-3xl">Andromeda</p>
+            <img
+              alt=""
+              className="rounded"
+              src="/static/img/maps/andromeda2.jpg"
+            />
+          </Card>
+          <Card className="mx-auto max-w-xl">
+            <p className=" py-3 text-center text-3xl">Ascent</p>
+            <img
+              alt=""
+              className="rounded"
+              src="/static/img/maps/ascent2.jpg"
+            />
+          </Card>
+          <Card className="mx-auto max-w-xl">
+            <p className=" py-3 text-center text-3xl">Blues</p>
+            <img
+              alt=""
+              className="rounded"
+              src="/static/img/maps/blues-v011.jpeg"
+            />
+          </Card>
+          <Card className="mx-auto max-w-xl">
+            <p className=" py-3 text-center text-3xl">Crash</p>
+            <img
+              alt=""
+              className="rounded"
+              src="/static/img/maps/ascent2.jpg"
+            />
+          </Card>
+          <Card className="mx-auto max-w-xl">
+            <p className=" py-3 text-center text-3xl">Deserted Valley</p>
+            <img
+              alt=""
+              className="rounded"
+              src="/static/img/maps/deserted-valley-overview.png"
+            />
+          </Card>
+          <Card className="mx-auto max-w-xl">
+            <p className=" py-3 text-center text-3xl">Eclipse</p>
+            <img
+              alt=""
+              className="rounded"
+              src="/static/img/maps/eclipse2.jpg"
+            />
+          </Card>
+          <Card className="mx-auto max-w-xl">
+            <p className=" py-3 text-center text-3xl">Fracture</p>
+            <img alt="" className="rounded" src="/static/img/maps/fra.jpg" />
+          </Card>
+          <Card className="mx-auto max-w-xl">
+            <p className=" py-3 text-center text-3xl">Incidamus</p>
+            <img alt="" className="rounded" src="/static/img/maps/incid.jpg" />
+          </Card>
+          <Card className="mx-auto max-w-xl">
+            <p className=" py-3 text-center text-3xl">Meridian</p>
+            <img
+              alt=""
+              className="rounded"
+              src="/static/img/maps/ascent2.jpg"
+            />
+          </Card>
+          <Card className="mx-auto max-w-xl">
+            <p className=" py-3 text-center text-3xl">Oceanus</p>
+            <img alt="" className="rounded" src="/static/img/maps/oce.jpg" />
+          </Card>
+          <Card className="mx-auto max-w-xl">
+            <p className=" py-3 text-center text-3xl">Periculo</p>
+            <img
+              alt=""
+              className="rounded"
+              src="/static/img/maps/periculo-features.png"
+            />
+          </Card>
+          <Card className="mx-auto max-w-xl">
+            <p className=" py-3 text-center text-3xl">Phlegethon</p>
+            <img
+              alt=""
+              className="rounded"
+              src="/static/img/maps/phlegathon-river.png"
+            />
+          </Card>
+          <Card className="mx-auto max-w-xl">
+            <p className=" py-3 text-center text-3xl">Polaris</p>
+            <img
+              alt=""
+              className="rounded"
+              src="/static/img/maps/polaris2.jpg"
+            />
+          </Card>
+          <Card className="mx-auto max-w-xl">
+            <p className=" py-3 text-center text-3xl">Styx</p>
+            <img alt="" className="rounded" src="/static/img/maps/sty.jpg" />
+          </Card>
+        </div>
+        <Separator />
+        <Card className="grow py-6 text-center">
+          <h1>Arena</h1>
+        </Card>
+
+        <div className="grid gap-3 sm:grid-cols-2">
+          <Card className="mx-auto max-w-xl">
+            <p className=" py-3 text-center text-3xl">Elysian Battleground</p>
+            <img alt="" className="rounded" src="/static/img/maps/eb.jpg" />
+          </Card>
+          <Card className="mx-auto max-w-xl">
+            <p className=" py-3 text-center text-3xl">Ruins</p>
+            <img alt="" className="rounded" src="/static/img/maps/ruins2.jpg" />
+          </Card>
+        </div>
+
+        <Separator />
+        <Card className="grow py-6 text-center">
+          <h1>CTF Blitz</h1>
+        </Card>
+
+        <div className="grid gap-3 sm:grid-cols-2">
+          <Card className="mx-auto max-w-xl">
+            <p className=" py-3 text-center text-3xl">Broadside</p>
+            <img
+              alt=""
+              className="rounded"
+              src="/static/img/maps/ascent2.jpg"
+            />
+          </Card>
+          <Card className="mx-auto max-w-xl">
+            <p className=" py-3 text-center text-3xl">Air Arena</p>
+            <img
+              alt=""
+              className="rounded"
+              src="/static/img/maps/ascent2.jpg"
+            />
+          </Card>
+        </div>
+
+        <Separator />
+        <Card className="grow py-6 text-center">
+          <h1>Miscellaneous Maps</h1>
+        </Card>
+
+        <div className="grid gap-3 sm:grid-cols-2">
+          <Card className="mx-auto max-w-xl">
+            <p className=" py-3 text-center text-3xl">Treacherous Pass</p>
+            <img
+              alt=""
+              className="rounded"
+              src="/static/img/maps/ascent2.jpg"
+            />
+          </Card>
+          <Card className="mx-auto max-w-xl">
+            <p className=" py-3 text-center text-3xl">Maze Runner</p>
+            <img
+              alt=""
+              className="rounded"
+              src="/static/img/maps/ascent2.jpg"
+            />
+          </Card>
+        </div>
       </div>
-
-      <SimpleGrid cols={{ base: 1, sm: 2, md: 3, xl: 4 }} className="gap-6 p-6">
-        <Card className="mx-auto max-w-xl">
-          <p className=" text-center text-3xl">Acheron</p>
-          <Image alt="" radius="md" src="/static/img/maps/acheron-river.png" />
-        </Card>
-        <Card className="mx-auto max-w-xl">
-          <p className=" text-center text-3xl">Andromeda</p>
-          <Image alt="" radius="md" src="/static/img/maps/andromeda2.jpg" />
-        </Card>
-        <Card className="mx-auto max-w-xl">
-          <p className=" text-center text-3xl">Ascent</p>
-          <Image alt="" radius="md" src="/static/img/maps/ascent2.jpg" />
-        </Card>
-        <Card className="mx-auto max-w-xl">
-          <p className=" text-center text-3xl">Blues</p>
-          <Image alt="" radius="md" src="/static/img/maps/blues-v011.jpeg" />
-        </Card>
-        <Card className="mx-auto max-w-xl">
-          <p className=" text-center text-3xl">Crash</p>
-          <Image alt="" radius="md" src="/static/img/maps/ascent2.jpg" />
-        </Card>
-        <Card className="mx-auto max-w-xl">
-          <p className=" text-center text-3xl">Deserted Valley</p>
-          <Image
-            alt=""
-            radius="md"
-            src="/static/img/maps/deserted-valley-overview.png"
-          />
-        </Card>
-        <Card className="mx-auto max-w-xl">
-          <p className=" text-center text-3xl">Eclipse</p>
-          <Image alt="" radius="md" src="/static/img/maps/eclipse2.jpg" />
-        </Card>
-        <Card className="mx-auto max-w-xl">
-          <p className=" text-center text-3xl">Fracture</p>
-          <Image alt="" radius="md" src="/static/img/maps/fra.jpg" />
-        </Card>
-        <Card className="mx-auto max-w-xl">
-          <p className=" text-center text-3xl">Incidamus</p>
-          <Image alt="" radius="md" src="/static/img/maps/incid.jpg" />
-        </Card>
-        <Card className="mx-auto max-w-xl">
-          <p className=" text-center text-3xl">Meridian</p>
-          <Image alt="" radius="md" src="/static/img/maps/ascent2.jpg" />
-        </Card>
-        <Card className="mx-auto max-w-xl">
-          <p className=" text-center text-3xl">Oceanus</p>
-          <Image alt="" radius="md" src="/static/img/maps/oce.jpg" />
-        </Card>
-        <Card className="mx-auto max-w-xl">
-          <p className=" text-center text-3xl">Periculo</p>
-          <Image
-            alt=""
-            radius="md"
-            src="/static/img/maps/periculo-features.png"
-          />
-        </Card>
-        <Card className="mx-auto max-w-xl">
-          <p className=" text-center text-3xl">Phlegethon</p>
-          <Image
-            alt=""
-            radius="md"
-            src="/static/img/maps/phlegathon-river.png"
-          />
-        </Card>
-        <Card className="mx-auto max-w-xl">
-          <p className=" text-center text-3xl">Polaris</p>
-          <Image alt="" radius="md" src="/static/img/maps/polaris2.jpg" />
-        </Card>
-        <Card className="mx-auto max-w-xl">
-          <p className=" text-center text-3xl">Styx</p>
-          <Image alt="" radius="md" src="/static/img/maps/sty.jpg" />
-        </Card>
-      </SimpleGrid>
-      <div className="max-w-8xl py-3 text-center text-6xl" color="green">
-        <h1 className="py-6">Arena</h1>
-      </div>
-
-      <SimpleGrid cols={{ base: 1, sm: 2 }} className=" gap-6 p-6">
-        <Card className="mx-auto max-w-xl">
-          <p className=" text-center text-3xl">Elysian Battleground</p>
-          <Image alt="" radius="md" src="/static/img/maps/eb.jpg" />
-        </Card>
-        <Card className="mx-auto max-w-xl">
-          <p className=" text-center text-3xl">Ruins</p>
-          <Image alt="" radius="md" src="/static/img/maps/ruins2.jpg" />
-        </Card>
-      </SimpleGrid>
-
-      <div className="max-w-8xl py-3 text-center text-6xl" color="green">
-        <h1 className="py-6">CTF Blitz</h1>
-      </div>
-
-      <SimpleGrid cols={{ base: 1, sm: 2 }} className=" gap-6 p-6">
-        <Card className="mx-auto max-w-xl">
-          <p className=" text-center text-3xl">Broadside</p>
-          <Image alt="" radius="md" src="/static/img/maps/ascent2.jpg" />
-        </Card>
-        <Card className="mx-auto max-w-xl">
-          <p className=" text-center text-3xl">Air Arena</p>
-          <Image alt="" radius="md" src="/static/img/maps/ascent2.jpg" />
-        </Card>
-      </SimpleGrid>
-
-      <div className="max-w-8xl py-3 text-center text-6xl" color="green">
-        <h1 className="py-6">Miscellaneous Maps</h1>
-      </div>
-
-      <SimpleGrid cols={{ base: 1, sm: 2 }} className=" gap-6 p-6">
-        <Card className="mx-auto max-w-xl">
-          <p className=" text-center text-3xl">Treacherous Pass</p>
-          <Image alt="" radius="md" src="/static/img/maps/ascent2.jpg" />
-        </Card>
-        <Card className="mx-auto max-w-xl">
-          <p className=" text-center text-3xl">Maze Runner</p>
-          <Image alt="" radius="md" src="/static/img/maps/ascent2.jpg" />
-        </Card>
-      </SimpleGrid>
     </>
   );
 }
