25061591774a48dddcee0f6e0b604cd08f825a4f
Dodge <toddbutler93@outlook.com.au>
2024-04-29 22:20:06 +1000

    Update T3 navbar for new pages

--- src/components/navbar-t3.tsx
+++ src/components/navbar-t3.tsx
@@ -4,7 +4,14 @@ import * as React from "react";
 import Link from "next/link";
 
 import { cn } from "~/lib/utils";
-import { FaGamepad } from "react-icons/fa6";
+import {
+  FaBars,
+  FaChevronRight,
+  FaGamepad,
+  FaGun,
+  FaSteam,
+} from "react-icons/fa6";
+import { FaEdit, FaHistory } from "react-icons/fa";
 import {
   NavigationMenu,
   NavigationMenuContent,
@@ -16,131 +23,270 @@ import {
 } from "./ui/navigation-menu";
 import { Separator } from "./ui/separator";
 import { ModeToggle } from "./theme-toggle";
+import { Button } from "./ui/button";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "./ui/collapsible";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet";
+import { ScrollArea } from "./ui/scroll-area";
 import Image from "next/image";
 
-const components: { title: string; href: string; description: string }[] = [
+const installMenuData = [
   {
-    title: "Alert Dialog",
-    href: "/docs/primitives/alert-dialog",
-    description:
-      "A modal dialog that interrupts the user with important content and expects a response.",
+    icon: FaSteam,
+    title: "Tribes 3 Steam Page",
+    description: "Purchase & play via steam!",
+    link: "https://store.steampowered.com/app/2687970/TRIBES_3_Rivals/",
   },
   {
-    title: "Hover Card",
-    href: "/docs/primitives/hover-card",
-    description:
-      "For sighted users to preview content available behind a link.",
+    icon: FaEdit,
+    title: "Configuration Guide",
+    description: "Configuring Tribes 3: Rivals!",
+    link: "/t3/docs/guide-config",
+  },
+];
+
+// const gameplayMenuData = [
+//   {
+//     icon: FaGun,
+//     title: "Offense",
+//     description: "[VSAA]",
+//     link: "/t3/docs/gameplay/guide-offense",
+//   },
+//   {
+//     icon: FaFlag,
+//     title: "Capping",
+//     description: "[VSAF]",
+//     link: "/t3/docs/gameplay/guide-cap",
+//   },
+//   {
+//     icon: FaWeightHanging,
+//     title: "Heavy on Flag",
+//     description: "Unavailable - Needs writing.", // [VSDF]
+//     link: "", // /t3/docs/gameplay/guide-hof
+//   },
+//   {
+//     icon: FaShield,
+//     title: "Defense",
+//     description: "Unavailable - Needs writing.", // [VSDB]
+//     link: "", // /t3/docs/gameplay/guide-defense
+//   },
+//   {
+//     icon: RxCrosshair1,
+//     title: "Chasing",
+//     description: "Unavailable - Needs writing.", // [VSDD]
+//     link: "", // /t3/docs/gameplay/guide-chase
+//   },
+// ];
+
+const tribesInfoMenuData = [
+  {
+    icon: FaHistory,
+    title: "Patch History",
+    description: "Tribes 3: Rivals Patch History",
+    link: "/docs/information/patch-history",
   },
   {
-    title: "Progress",
-    href: "/docs/primitives/progress",
-    description:
-      "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
+    icon: FaGun,
+    title: "Classes",
+    description: "Tribes 3: Rivals Weapon Information",
+    link: "/t3/docs/information/classes",
   },
   {
-    title: "Scroll-area",
-    href: "/docs/primitives/scroll-area",
-    description: "Visually or semantically separates content.",
-  },
-  {
-    title: "Tabs",
-    href: "/docs/primitives/tabs",
-    description:
-      "A set of layered sections of content—known as tab panels—that are displayed one at a time.",
-  },
-  {
-    title: "Tooltip",
-    href: "/docs/primitives/tooltip",
-    description:
-      "A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.",
+    icon: FaGun,
+    title: "Weapons",
+    description: "Tribes 3: Rivals Weapon Information",
+    link: "/t3/docs/information/weapons",
   },
 ];
 
 export function NavBarT3() {
   return (
     // TODO: Header Background/text variables
-    <div className="sticky top-0 z-50 flex h-14 w-full flex-row justify-between bg-inherit">
-      <Link href={"/"} className="flex flex-row items-center gap-3 p-6">
+    <div className="sticky top-0 z-50 flex h-[60px] w-screen justify-between gap-3 bg-gradient-to-b from-orange-500 to-red-900 px-3">
+      <Link href={"/t3"} className="flex flex-row items-center gap-3 pe-3">
         <Image
           width={500}
           height={500}
-          src="https://utfs.io/f/a1dd52b8-5b9e-432d-9a7b-b713f2aaf928-whylmk.png"
           alt=""
-          className="h-8 w-8"
+          src="https://utfs.io/f/a1dd52b8-5b9e-432d-9a7b-b713f2aaf928-whylmk.png"
+          className="h-8 w-8 min-w-8"
         />
-        <h1 className=" invisible md:visible">Dodges Domain</h1>
+        <h4 className="text-nowrap bg-transparent py-3 text-zinc-200 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,1)]  max-xl:hidden">
+          Dodges Domain
+        </h4>
+        <h4 className="text-nowrap bg-transparent py-3 text-zinc-200 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,1)] lg:hidden">
+          Dodges Domain
+        </h4>
       </Link>
-      <NavigationMenu className=" invisible justify-center sm:visible">
-        <NavigationMenuList>
+      <NavigationMenu className="max-lg:hidden">
+        <NavigationMenuList className=" text-zinc-200 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,1)]">
           <NavigationMenuItem>
-            <NavigationMenuTrigger>Getting started</NavigationMenuTrigger>
+            <NavigationMenuTrigger className="bg-transparent">
+              Installation
+            </NavigationMenuTrigger>
             <NavigationMenuContent>
-              <div className="gap-3">
-                <div className="flex flex-row justify-between p-6">
-                  <h1>Tribes Installation</h1>
-                </div>
-                <Separator />
+              <div className="flex flex-row justify-between p-6">
+                <h3>Tribes 3 Installation</h3>
               </div>
-
-              <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
-                <li className="row-span-3">
-                  <NavigationMenuLink asChild>
-                    <Link
-                      className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
-                      href="/"
-                    >
-                      <FaGamepad className="h-6 w-6" />
-                      <div className="mb-2 mt-4 text-lg font-medium">
-                        shadcn/ui
-                      </div>
-                      <p className="text-sm leading-tight text-muted-foreground">
-                        Beautifully designed components that you can copy and
-                        paste into your apps. Accessible. Customizable. Open
-                        Source.
-                      </p>
-                    </Link>
-                  </NavigationMenuLink>
-                </li>
-                <ListItem href="/docs" title="Introduction">
-                  <FaGamepad></FaGamepad>Re-usable components built using Radix
-                  UI and Tailwind CSS.
-                </ListItem>
-                <ListItem href="/docs/installation" title="Installation">
-                  How to install dependencies and structure your app.
-                </ListItem>
-                <ListItem href="/docs/primitives/typography" title="Typography">
-                  Styles for headings, paragraphs, lists...etc
-                </ListItem>
-              </ul>
-            </NavigationMenuContent>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <NavigationMenuTrigger>Components</NavigationMenuTrigger>
-            <NavigationMenuContent>
-              <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
-                {components.map((component) => (
+              <Separator />
+              <ul className="grid w-[400px] gap-3 p-6 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+                {installMenuData.map((component) => (
                   <ListItem
                     key={component.title}
                     title={component.title}
-                    href={component.href}
+                    href={component.link}
                   >
-                    {component.description}
+                    <div className="flex flex-row flex-nowrap items-center gap-3 transition-all duration-300 hover:text-teal-500 active:text-teal-500">
+                      <component.icon
+                        color="teal"
+                        className="h-10 w-10 bg-zinc-800 p-1"
+                      />
+                      <div className="flex flex-col">
+                        <p className="text-sm">{component.title}</p>
+                        <p className="text-xs">{component.description}</p>
+                      </div>
+                    </div>
                   </ListItem>
                 ))}
               </ul>
             </NavigationMenuContent>
           </NavigationMenuItem>
+
           <NavigationMenuItem>
-            <Link href="/docs" legacyBehavior passHref>
-              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                Documentation
+            <Link href="/t3/docs/gameplay/guide-quick" legacyBehavior passHref>
+              <NavigationMenuLink
+                className={navigationMenuTriggerStyle() + " bg-transparent"}
+              >
+                Competitive Guide
               </NavigationMenuLink>
             </Link>
           </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link href="/t3/docs/maps" legacyBehavior passHref>
+              <NavigationMenuLink
+                className={navigationMenuTriggerStyle() + " bg-transparent"}
+              >
+                Maps
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger className="bg-transparent">
+              Game Information
+            </NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <div className="gap-3">
+                <div className="flex flex-row justify-between p-6">
+                  <h3>Game Information</h3>
+                </div>
+                <Separator />
+              </div>
+              <ul className="grid w-[400px] gap-3 p-6 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+                {tribesInfoMenuData.map((component) => (
+                  <ListItem
+                    key={component.title}
+                    title={component.title}
+                    href={component.link}
+                  >
+                    <div className="flex flex-row flex-nowrap items-center gap-3 transition-all duration-300 hover:text-teal-500 active:text-teal-500">
+                      <component.icon
+                        color="teal"
+                        className="h-10 w-10 bg-zinc-800 p-1"
+                      />
+                      <div className="flex flex-col">
+                        <p className="text-sm">{component.title}</p>
+                        <p className="text-xs">{component.description}</p>
+                      </div>
+                    </div>
+                  </ListItem>
+                ))}
+              </ul>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
         </NavigationMenuList>
       </NavigationMenu>
-      <div className="flex items-center justify-end p-6">
+      <div className="flex items-center gap-3 pe-3">
         <ModeToggle />
+        <div className="flex items-center lg:hidden">
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button
+                className="flex flex-col items-center justify-center"
+                size="icon"
+                variant="outline"
+              >
+                <FaBars className="h-6 w-6" />
+              </Button>
+            </SheetTrigger>
+            <SheetContent side="top">
+              <ScrollArea className="h-[720px]">
+                <SheetHeader>
+                  <SheetTitle className="pb-6">Dodges Domain</SheetTitle>
+                </SheetHeader>
+                <div className="flex w-full items-center justify-center gap-3 rounded-lg bg-gradient-to-b from-amber-400 to-red-500 p-3 text-lg font-semibold text-zinc-950 hover:text-zinc-950">
+                  <Link href="/">Looking for Tribes Ascend?</Link>
+                </div>
+                <Collapsible className="grid gap-3 pe-6 pt-6">
+                  <CollapsibleTrigger className="flex w-full items-center text-lg font-semibold [&[data-state=open]>svg]:rotate-90">
+                    Installation Guides
+                    <FaChevronRight className="ml-auto h-5 w-5 transition-all" />
+                  </CollapsibleTrigger>
+                  {installMenuData.map((component) => (
+                    <CollapsibleContent key={component.title}>
+                      <Link
+                        className="flex w-full items-center text-lg font-semibold"
+                        href={component.link}
+                      >
+                        <div className="flex grow flex-row items-center gap-6 rounded p-3 hover:bg-accent">
+                          <component.icon className="h-10 w-10 rounded p-2 text-primary dark:bg-zinc-800" />
+                          <div className="flex flex-row gap-2">
+                            {component.title}
+                          </div>
+                        </div>
+                      </Link>
+                    </CollapsibleContent>
+                  ))}
+                </Collapsible>
+                <div className="flex w-full items-center gap-3 pe-6 pt-6 text-lg font-semibold">
+                  <Link href="t3/docs/gameplay/guide-quick">Competitive Guide</Link>
+                </div>
+                <div className="flex w-full items-center gap-3 pe-6 pt-6 text-lg font-semibold">
+                  <Link href="t3/docs/maps">Maps</Link>
+                </div>
+                <Collapsible className="grid gap-3 pe-6 pt-6">
+                  <CollapsibleTrigger className="flex w-full items-center text-lg font-semibold [&[data-state=open]>svg]:rotate-90">
+                    Tribes Information
+                    <FaChevronRight className="ml-auto h-5 w-5 transition-all" />
+                  </CollapsibleTrigger>
+                  {tribesInfoMenuData.map((component) => (
+                    <CollapsibleContent key={component.title}>
+                      <Link
+                        className="flex w-full items-center text-lg font-semibold"
+                        href={component.link}
+                      >
+                        <div className="flex grow flex-row items-center gap-6 rounded p-3 hover:bg-accent">
+                          <component.icon className="h-10 w-10 rounded p-2 text-primary dark:bg-zinc-800" />
+                          <div className="flex flex-row gap-2">
+                            {component.title}
+                          </div>
+                        </div>
+                      </Link>
+                    </CollapsibleContent>
+                  ))}
+                </Collapsible>
+              </ScrollArea>
+            </SheetContent>
+          </Sheet>
+        </div>
       </div>
     </div>
   );
@@ -151,7 +297,7 @@ const ListItem = React.forwardRef<
   React.ComponentPropsWithoutRef<"a">
 >(({ className, title, children, ...props }, ref) => {
   return (
-    <li>
+    <li key={title}>
       <NavigationMenuLink asChild>
         <a
           ref={ref}
@@ -161,10 +307,7 @@ const ListItem = React.forwardRef<
           )}
           {...props}
         >
-          <div className="text-sm font-medium leading-none">{title}</div>
-          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
-            {children}
-          </p>
+          {children}
         </a>
       </NavigationMenuLink>
     </li>
