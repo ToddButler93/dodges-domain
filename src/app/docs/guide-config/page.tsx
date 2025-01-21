22b9e8411e5519ec0271097820913dc19bbfb569
Dodge <toddbutler93@outlook.com.au>
2025-01-21 12:01:53 +1100

    Shorten config guide

--- src/app/docs/guide-config/page.tsx
+++ src/app/docs/guide-config/page.tsx
@@ -9,7 +9,7 @@ export default function GuideConfig() {
     <>
       <div className="flex flex-col gap-6">
         <Card className="grow p-6 text-center">
-          <h1>Tribes Ascend Configuration</h1>
+          <h1>Tribes Ascend Config</h1>
         </Card>
         <div className="flex flex-col gap-6">
           <div className="flex flex-col gap-6">
