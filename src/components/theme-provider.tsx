1cc2d70d6c67c21002ab6e1672062f03c2a7278f
Dodge <toddbutler93@outlook.com.au>
2025-01-22 11:01:28 +1100

    Fix hydration error on theme-provider

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -2,8 +2,18 @@
 
 import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { useEffect, useState } from "react";
 import { type ThemeProviderProps } from "next-themes/dist/types";
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
