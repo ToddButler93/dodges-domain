7ab582f7f4216c9858347b306030dc60ed489be6
Dodge <toddbutler93@outlook.com.au>
2025-01-22 11:35:13 +1100

    Fix typing for theme-provider

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -3,7 +3,8 @@
 import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { useEffect, useState } from "react";
-import { type ThemeProviderProps } from "next-themes/dist/types";
+
+type ThemeProviderProps = React.ComponentProps<typeof NextThemesProvider>;
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   const [mounted, setMounted] = useState(false);
