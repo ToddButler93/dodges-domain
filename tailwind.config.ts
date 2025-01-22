6b4405eaa4158725cf280bb2b1552ed16dbf81b0
Dodge <toddbutler93@outlook.com.au>
2025-01-22 11:35:01 +1100

    Fix Tailwind config duplicates

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -71,22 +71,6 @@ export default {
             height: "0",
           },
         },
-        "accordion-down": {
-          from: {
-            height: "0",
-          },
-          to: {
-            height: "var(--radix-accordion-content-height)",
-          },
-        },
-        "accordion-up": {
-          from: {
-            height: "var(--radix-accordion-content-height)",
-          },
-          to: {
-            height: "0",
-          },
-        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
